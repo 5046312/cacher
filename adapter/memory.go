8c3093a5199d88cd2e769288b414b2220e2c1171
5046312 <5046312@qq.com>
2019-05-22 15:37:41 +0800

    TODO forever save

--- adapter/memory.go
+++ adapter/memory.go
@@ -11,7 +11,7 @@ const (
 
 // Memory Adapter
 type MemoryCacher struct {
-	sync.Mutex
+	sync.RWMutex
 	Caches  map[string]*CacheItem
 	GC_Time time.Duration
 }
@@ -22,25 +22,53 @@ func DefaultMemoryCacher() *MemoryCacher {
 
 func NewMemoryCacher(gcTime time.Duration) *MemoryCacher {
 	return &MemoryCacher{
-		Caches:  map[string]*CacheItem{},
+		Caches:  make(map[string]*CacheItem),
 		GC_Time: gcTime,
 	}
 }
 
+// 检测key是否过期
+// 过期则删除，返回 true
+// 未过期，返回false
+func (mc *MemoryCacher) isExp(item *CacheItem) bool {
+	if item.Exp.Before(time.Now()) {
+		// 过期，删除
+		delete(mc.Caches, item.Key)
+		return true
+	}
+	return false
+}
+
+//
 func (mc *MemoryCacher) Get(key string) interface{} {
+	mc.RLock()
+	defer mc.RUnlock()
+	if item, ok := mc.Caches[key]; ok {
+		// 存在数据，判断是否过期
+		if mc.isExp(item) {
+			return nil
+		}
+		return item.Data
+	}
 	return nil
 }
-func (mc *MemoryCacher) Set(key string, val interface{}, exp time.Duration) error {
+
+//
+func (mc *MemoryCacher) Set(key string, val interface{}, timeout time.Duration) error {
+	mc.Caches[key] = &CacheItem{key, val, time.Now().Add(timeout * time.Second)}
 	return nil
 }
 func (mc *MemoryCacher) Remove(key string) error {
+	delete(mc.Caches, key)
 	return nil
 }
 func (mc *MemoryCacher) Pull(key string) interface{} {
-	return nil
+	val := mc.Get(key)
+	mc.Remove(key)
+	return val
 
 }
 func (mc *MemoryCacher) Clear() error {
+	mc.Caches = make(map[string]*CacheItem)
 	return nil
-
 }
